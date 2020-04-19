f9e29d728281bba6ea773197de36443e138d7773
giribabu-g <giribabu.g@mtwlabs.com>
2020-04-19 21:42:57 +0530

    proj4

--- src/app/australia/australia.component.ts
+++ src/app/australia/australia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-australia',
@@ -7,13 +8,32 @@ import { User } from './user';
   styleUrls: ['./australia.component.css']
 })
 export class AustraliaComponent implements OnInit {
+  public userForm:NgForm;
+  public data:any;
   
  topics = ['java','Angular','oracle'];
-userModel = new User(26,'sai','sri',new Date(),'','nvrsai@gmail.com',950208732,'morning',true);
-submitted=false;
+ aHasError = true;
 
-constructor( )
-  {}
+userModel={
+  fname:"",
+  sname:"",
+  date:"",
+  a:"",
+  email:"",
+  phonenumber:""
+}
+ 
+validateA(value){
+  if(value=='default'){
+    this.aHasError=true;
+  }
+  else{
+    this.aHasError=false;
+  }
+}
+ 
+// constructor( )
+//   {}
 
   // model={
   //   firstName:"" ,
@@ -41,11 +61,32 @@ constructor( )
 
   // }
   ngOnInit() {
+   
+    this.responseData();
+
   }
 // submit(){
 //     if(this.model)
 //     console.log(this.model)
 
 // }
+onSubmit(){
+  // console.log(this.userModel);
+localStorage.setItem('registerdata',JSON.stringify(this.userModel))
 
+}
+responseData(){
+  this.data=JSON.parse(localStorage.getItem('registerdata'));
+console.log(this.data);
+if(this.data){
+  this.userModel.fname=this.data.fname,
+  this.userModel.sname=this.data.sname,
+  this.userModel.date=this.data.date,
+  this.userModel.a=this.data.a,
+  this.userModel.email=this.data.email,
+  this.userModel.phonenumber=this.data.phonenumber
+
+}
+
+}
 }
--- src/app/australia/user.ts
+++ src/app/australia/user.ts
@@ -1,6 +1,6 @@
 export class User {
     constructor(
-    public id:number,
+    
     public fname: string,
     public sname: string,
     public date : Date,
